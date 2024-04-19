8b6c1a291c15ec01e939bc400a3a7fbcbbf812bd
Ahdad0 <abderrahimahdadabdo@gmail.com>
2024-04-19 17:46:36 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,9 +11,13 @@ def number_of_subscribers(subreddit):
     if subreddit is None or type(subreddit) is not str:
         return 0
 
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-                             v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {
+        'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'
+    }
+
+    r = requests.get(url, headers=headers).json()
 
     subs = r.get("data", {}).get("subscribers", 0)
 
